544eb7578b075f9ef7040f331e62ed5da275f057
nmbngoc <1751085@student.hcmus.edu.vn>
2020-12-01 14:41:03 +0700

    ServerUI

--- src/sample/Main3/Main.java
+++ src/sample/Main3/Main.java
@@ -4,23 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+import java.io.IOException;
 
 public class Main extends Application {
+    //public ServerController ser;
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("Server.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            Parent root = FXMLLoader.load(this.getClass().getResource("sample.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
---
+++ src/sample/Main3/ServerController.java
@@ -0,0 +1,39 @@
+package sample.Main3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.text.Text;
+
+
+public class ServerController {
+    @FXML
+    //private TextArea log;
+    public ScrollPane sp;
+    /*
+    public ServerController() {
+        sp = new ScrollPane();
+    }*/
+    @FXML
+    private void initialize()
+    {
+        Text txt = new Text("The look and feel of JavaFX applications "
+                + "can be customized. Cascading Style Sheets (CSS) separate "
+                + "appearance and style from implementation so that developers can "
+                + "concentrate on coding. Graphic designers can easily "
+                + "customize the appearance and style of the application "
+                + "through the CSS. If you have a web design background,"
+                + " or if you would like to separate the user interface (UI) "
+                + "and the back-end logic, then you can develop the presentation"
+                + " aspects of the UI in the FXML scripting language and use Java "
+                + "code for the application logic. If you prefer to design UIs "
+                + "without writing code, then use JavaFX Scene Builder. As you design the UI, "
+                + "Scene Builder creates FXML markup that can be ported to an Integrated Development "
+                + "Environment (IDE) so that developers can add the business logic.");
+        txt.wrappingWidthProperty().bind(sp.widthProperty());
+        sp.setContent(txt);
+    }
+
+
+
+}
